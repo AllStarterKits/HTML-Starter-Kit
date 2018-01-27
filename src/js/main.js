4bb448b3f0936e81ea4637e528327a7ffd0d3055
Luciano Nooijen <lucianonooijen@gmail.com>
2018-01-27 20:45:05 +0100

    Added ESDoc

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+/**
+ * This is a function that will console.log that this script is added properly
+ * @todo remove this function
+ * @since v0.0.0
+ */
+
 $(document).ready(function(){
   let workingJS = () => {
     console.log('Custom Javascript loaded');
